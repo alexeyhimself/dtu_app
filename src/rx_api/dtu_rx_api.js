51d7cbecc7e78676171b2f21f7d6b1a0df61ea2e
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2023-05-07 01:03:02 +0200

    Trying RX API

--- src/rx_api/dtu_rx_api.js
+++ src/rx_api/dtu_rx_api.js
@@ -23,4 +23,28 @@ function DTU_RX_API_submint_report_endpoint(report) {
   RX_API_save_to_db(report);
 }
 
-console.log("make element path to lower case both for rx and tx apis")
+console.warn("make element path to lower case both for rx and tx apis")
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+async function DTU_RX_API_submint_report(report) {
+  // Default options are marked with *
+  const response = await fetch('localhost/rx', {
+    method: "POST",
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(report), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+/*
+postData("https://example.com/answer", { answer: 42 }).then((data) => {
+  console.log(data); // JSON data parsed by `data.json()` call
+});
+*/
